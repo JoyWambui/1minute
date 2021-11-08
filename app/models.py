2a5cf4c2f99ad170075d7285c8311a4bcb9258d5
JoyWambui <JoyWambui@github.com>
2021-11-08 22:56:58 +0300

    Create user model.

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from . import db
+
+class User(db.Model):
+    """Class that defines a User Model and helps us create new users."""
+    __tablename__ = "users"
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
+    
