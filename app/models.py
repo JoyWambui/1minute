0991df9639b4f41742f287c2c85fcd1e61c3b87f
JoyWambui <JoyWambui@github.com>
2021-11-08 23:54:49 +0300

    Add databse migrations.

--- app/models.py
+++ app/models.py
@@ -5,19 +5,9 @@ class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return f'User {self.username}'
     
-class Role(db.Model):
-    """Role class that will define all the different roles"""
-    __tablename__ = 'roles'
 
-    id = db.Column(db.Integer,primary_key = True)
-    role_type = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
-
-    def __repr__(self):
-        return f'User {self.name}'
     
