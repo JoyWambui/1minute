7c4a869c6ee137003cd68597012ee689ce862bdc
JoyWambui <JoyWambui@github.com>
2021-11-09 07:05:00 +0300

    Create authentication blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -11,5 +11,7 @@ def create_app(config_name):
     db.init_app(app)
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    from .auth import authentication as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix="/authentication")
     
     return app
