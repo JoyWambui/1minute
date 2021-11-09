928533b78dee2f9c43edfd7b360b8703b1b80dd3
JoyWambui <JoyWambui@github.com>
2021-11-09 07:13:50 +0300

    Initialize flask-login

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,22 @@ from re import I
 from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection="strong"
+login_manager.login_view="authentication.login"
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
+    
+    #Initializing flask extensions
     db.init_app(app)
+    login_manager.init_app(app)
+    
+    #Registering blueprints
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     from .auth import authentication as auth_blueprint
