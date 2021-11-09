efe88c98fe71283888406ec777f942498b93ffbe
JoyWambui <JoyWambui@github.com>
2021-11-09 17:23:17 +0300

    add markdown editor

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,13 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 bootsrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 mail = Mail()
+simple = SimpleMDE()
 
 login_manager.session_protection="strong"
 login_manager.login_view="authentication.login"
@@ -22,7 +24,7 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
-    
+    simple.init_app(app)
     #Registering blueprints
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
