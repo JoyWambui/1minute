a50b586e36dd5f7fc44ed16e9d71c0278ccc0ed0
JoyWambui <JoyWambui@github.com>
2021-11-09 14:45:03 +0300

    Create user profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from . import main
+from ..models import User
 #from flask_login import login_required
 #@main.route('/pitch/comment/new/<int:id>', methods = ['GET','POST'])
 #@login_required
@@ -9,3 +10,12 @@ from . import main
 def index():
     """View root page function that returns the index page and its data"""
     return render_template("index.html")
+
+@main.route('/user/<username>')
+def my_profile(username):
+    user = User.query.filter_by(username = username).first()
+
+    if user is None:
+        abort(404)
+    title="Profile Page"
+    return render_template("profile/user_profile.html", user=user,title=title)
